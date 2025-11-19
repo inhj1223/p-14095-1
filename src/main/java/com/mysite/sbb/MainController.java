891c27defd50ef2e2a303396530c3162e53b4386
장인호 <jih95349238@gmail.com>
2025-11-19 15:21:06 +0900

    ~

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,16 +2,21 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-    @GetMapping("/s")
-    public void index(){
-        System.out.println("index");
+    @GetMapping("/")
+    @ResponseBody
+    public String index(){
+        System.out.println("index() 메서드 호출됨!");
+
+        return "메롱";
     }
 
     @GetMapping("/hello")
-    public void hello(){
-        System.out.println("hello() 메서드 호출됨!");
+    @ResponseBody
+    public String  hello(){
+        return "안녕하세요";
     }
 }
